8d8ceca695b002fe4d6ef9afa6216dddec823b84
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-11-18 13:15:47 -0600

    lbryum fixes

--- lbryum-server/run.sh
+++ lbryum-server/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-lbrycrdd -datadir=/data/lbrycrd &
+# lbrycrdd is set to daemon-fy
+/usr/local/bin/lbrycrdd -datadir=/data/lbrycrd
 runuser -l lbryum -c 'run_lbryum_server.py --conf=/etc/lbryum.conf'
