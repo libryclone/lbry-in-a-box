93826abc8e38abbf1791cc3481e93234018a845d
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-11-18 16:17:02 -0600

    bug fixes in submodules

--- lbryum-server/run.sh
+++ lbryum-server/run.sh
@@ -2,4 +2,10 @@
 
 # lbrycrdd is set to daemon-fy
 /usr/local/bin/lbrycrdd -datadir=/data/lbrycrd
+# force the server to reload from lbrycrd each time
+#
+# for some reason, on restart I'm getting a lot of
+# {u'message': u"Can't read block from disk", u'code': -32603}
+# errors from lbrycrd. Putting in a clean start seems to fix it.
+rm -rf /data/lbryum-db
 runuser -l lbryum -c 'run_lbryum_server.py --conf=/etc/lbryum.conf'
