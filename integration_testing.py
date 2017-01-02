ac33b2753df4f8a69987f37952b6871ecc1cb448
Kay Kurokawa <kay@lbry.io>
2017-01-02 16:18:00 -0500

    test publish/download with key fee, some DRY fixes, sync to tag option

--- integration_testing.py
+++ integration_testing.py
@@ -2,6 +2,9 @@
 
 Integration testing using lbry-in-a-box
 
+This will stop,rebuild and launch docker containers for lbry in a box,
+and than run integration testing
+
 """
 
 import unittest
@@ -19,9 +22,11 @@ lbrynet_rpc_port = '5279'
 dht_rpc_port = '5278'
 reflector_rpc_port = '5277'
 
-lbrynet = JSONRPCProxy.from_url("http://localhost:{}/lbryapi".format(lbrynet_rpc_port))
-dht = JSONRPCProxy.from_url("http://localhost:{}/lbryapi".format(dht_rpc_port))
-reflector = JSONRPCProxy.from_url("http://localhost:{}/lbryapi".format(reflector_rpc_port))
+lbrynets={}
+lbrynets['lbrynet'] = JSONRPCProxy.from_url("http://localhost:{}/lbryapi".format(lbrynet_rpc_port))
+lbrynets['dht'] = JSONRPCProxy.from_url("http://localhost:{}/lbryapi".format(dht_rpc_port))
+lbrynets['reflector'] = JSONRPCProxy.from_url("http://localhost:{}/lbryapi".format(reflector_rpc_port))
+DOCKER_LOG_FILE='tmp.log' 
 
 
 def shell_command(command):
@@ -35,14 +40,23 @@ def call_lbrycrd(method,*params):
                                                             lbrycrd_rpc_ip,lbrycrd_rpc_port))
     return getattr(lbrycrd,method)(*params)
 
+#wrapper function just to see where we are in the test
+def print_func(func):
+    def wrapper(*args,**kwargs):
+        print("Running:{}".format(func.func_name))
+        return func(*args,**kwargs)
+    return wrapper
 
 class LbrynetTest(unittest.TestCase):
 
     def test_lbrynet(self):
         self._test_lbrynet_startup()
-        self._test_recv()
-        self._test_send()
-        self._test_publish()
+        self._test_recv_and_send()
+        self._test_publish('testname')
+        self._test_publish('testname2',1)
+        print("Printing ERRORS found in log:")
+        out,err=shell_command('grep ERROR {}'.format(DOCKER_LOG_FILE))
+        print out
 
     def _increment_blocks(self, num_blocks):
         LBRYNET_BLOCK_SYNC_TIMEOUT = 60
@@ -58,10 +72,7 @@ class LbrynetTest(unittest.TestCase):
         self._is_blockhash(best_block_hash)
         start_time = time.time()
         while time.time() - start_time < LBRYNET_BLOCK_SYNC_TIMEOUT:
-            lbrynet_synced = lbrynet.get_best_blockhash() == best_block_hash
-            dht_synced = dht.get_best_blockhash() == best_block_hash
-            reflector_synced = reflector.get_best_blockhash() == best_block_hash
-            if lbrynet_synced and dht_synced and reflector_synced:
+            if all([lbrynet.get_best_blockhash() == best_block_hash for lbrynet in lbrynets.values()]):
                 return
             time.sleep(0.01)
         self.fail('Lbrynet block sync timed out')
@@ -72,55 +83,76 @@ class LbrynetTest(unittest.TestCase):
     def _is_blockhash(self, blockhash):
         self.assertEqual(len(blockhash),64)
 
-    def _wait_for_lbrynet_sync(self):
-        time.sleep(60)
+    def _wait_till_balance_equals(self,lbrynet,amount):
+        LBRYNET_SYNC_TIMEOUT = 30
+        start_time = time.time()
+        while time.time() - start_time < LBRYNET_SYNC_TIMEOUT:
+            if lbrynet.get_balance() == amount:
+                return
+            time.sleep(0.01)
+        self.fail('Lbrynet failed to sync balance in time')
 
-    # wait and test to see if lbrynet startups
+    def _wait_for_lbrynet_sync(self):
+        time.sleep(20)
+
+    # send amount from lbrycrd to lbrynet instance
+    def _send_from_lbrycrd(self, amount, to_lbrynet):
+        LBRYNET_RECV_TIMEOUT = 60
+        prev_balance = to_lbrynet.get_balance()
+        address = to_lbrynet.get_new_address()
+        out = call_lbrycrd('sendtoaddress',address,amount)
+        self._is_txid(out)
+        self._increment_blocks(6)
+        start_time = time.time()
+        self._wait_till_balance_equals(to_lbrynet,prev_balance+amount)
+
+
+    def _check_lbrynet_init(self,lbrynet):
+        if lbrynet.is_running():
+            self.assertEqual(0,lbrynet.get_balance())
+            self.assertEqual(True,lbrynet.is_first_run())
+            return True
+        else:
+            return False
+
+    @print_func
     def _test_lbrynet_startup(self):
         LBRYNET_STARTUP_TIMEOUT = 60
         start_time = time.time()
         while time.time() - start_time < LBRYNET_STARTUP_TIMEOUT:
-            if lbrynet.is_running():
-                print("Lbrynet startup detected")
-                self.assertEqual(0, lbrynet.get_balance())
-                self.assertEqual(True, lbrynet.is_first_run())
+            if all([self._check_lbrynet_init(lbrynet) for lbrynet in lbrynets.values()]):
                 return
             time.sleep(0.01)
 
         self.fail('Lbrynet failed to start up')
 
     # receive balance from lbrycrd to lbrynet
-    def _test_recv(self):
+    @print_func
+    def _test_recv_and_send(self):
         RECV_AMOUNT = 10
-        address = lbrynet.get_new_address()
-        out = call_lbrycrd('sendtoaddress',address,RECV_AMOUNT)
-        self._is_txid(out)
-        self._increment_blocks(6)
-        self._wait_for_lbrynet_sync()
-        balance = lbrynet.get_balance()
-        self.assertEqual(RECV_AMOUNT, balance)
-
-    #send balance from lbrynet back to lbrycrd
-    def _test_send(self):
         SEND_AMOUNT = 1
+        self._send_from_lbrycrd(RECV_AMOUNT,lbrynets['lbrynet'])
+
         # create lbrycrd address
         address = call_lbrycrd('getnewaddress','test')
         out = call_lbrycrd('getbalance','test')
         self.assertEqual(0,out)
 
         # send from lbrynet to lbrycrd
-        out = lbrynet.send_amount_to_address({'amount':SEND_AMOUNT, 'address':address})
+        out = lbrynets['lbrynet'].send_amount_to_address({'amount':SEND_AMOUNT, 'address':address})
         self.assertEqual(out,True)
         self._wait_for_lbrynet_sync()
         self._increment_blocks(6)
         out = call_lbrycrd('getbalance', 'test')
         self.assertEqual(SEND_AMOUNT, out)
 
-    # test publishing from lbrynet, and test to see if we can download from dht 
-    def _test_publish(self):
+
+    # test publishing from lbrynet, and test to see if we can download from dht
+    @print_func
+    def _test_publish(self,claim_name,key_fee = 0):
         CLAIM_AMOUNT = 1
-        # make sure we have enough amount
-        out = lbrynet.get_balance()
+        # make sure we have enough to claim the amount
+        out = lbrynets['lbrynet'].get_balance()
         self.assertTrue(out >= CLAIM_AMOUNT)
 
         test_metadata = {
@@ -134,7 +166,11 @@ class LbrynetTest(unittest.TestCase):
             'content_type': 'video/mp4',
             'thumbnail': 'test'
         }
-        out = lbrynet.publish({'name':'testname','file_path':'/src/lbry/FAQ.md','bid':CLAIM_AMOUNT,'metadata':test_metadata})
+        if key_fee != 0:
+            key_fee_address = lbrynets['lbrynet'].get_new_address()
+            test_metadata["fee"]= {'LBC': {"address": key_fee_address, "amount": key_fee}}
+
+        out = lbrynets['lbrynet'].publish({'name':claim_name,'file_path':'/src/lbry/FAQ.md','bid':CLAIM_AMOUNT,'metadata':test_metadata})
         publish_txid = out['txid']
         publish_nout = out['nout']
         self._is_txid(publish_txid)
@@ -143,16 +179,18 @@ class LbrynetTest(unittest.TestCase):
         self._wait_for_lbrynet_sync()
         self._increment_blocks(6)
 
+        balance_before_key_fee = lbrynets['lbrynet'].get_balance()
+
         # check lbrycrd claim state is updated
-        out = call_lbrycrd('getvalueforname','testname')
+        out = call_lbrycrd('getvalueforname',claim_name)
         self.assertEqual(out['amount'],CLAIM_AMOUNT*100000000)
         self.assertEqual(out['effective amount'],CLAIM_AMOUNT*100000000)
         self.assertEqual(out['txid'],publish_txid)
         self.assertEqual(out['n'],publish_nout)
 
         # check lbrynet claim state is updated
-        out = lbrynet.get_claim_info({'name':'testname'})
-        self.assertEqual('testname', out['name'])
+        out = lbrynets['lbrynet'].get_claim_info({'name':claim_name})
+        self.assertEqual(claim_name, out['name'])
         self.assertEqual(publish_txid, out['txid'])
         self.assertEqual(publish_nout, out['nout'])
         self.assertEqual(CLAIM_AMOUNT, out['amount'])
@@ -161,41 +199,51 @@ class LbrynetTest(unittest.TestCase):
         sd_hash = out['value']['sources']['lbry_sd_hash']
 
         # test download of own file
-        out = lbrynet.get({'name':'testname'})
+        out = lbrynets['lbrynet'].get({'name':claim_name})
         self.assertEqual('/data/Downloads/FAQ.md',out['path'])
         self.assertEqual(sd_hash, out['stream_hash'])
 
         # check to see if we can access sd_hash
-        out = lbrynet.download_descriptor({'sd_hash':sd_hash})
+        out = lbrynets['lbrynet'].download_descriptor({'sd_hash':sd_hash})
         blob_hash = out['blobs'][0]['blob_hash']
 
         # check that we have all the hashes
-        out = lbrynet.get_blob_hashes()
+        out = lbrynets['lbrynet'].get_blob_hashes()
         self.assertTrue(sd_hash in out)
         self.assertTrue(blob_hash in out)
 
         # check reflector to see if it can access sd_hash
-        out = reflector.download_descriptor({'sd_hash':sd_hash})
+        out = lbrynets['reflector'].download_descriptor({'sd_hash':sd_hash})
         self.assertEqual(blob_hash,out['blobs'][0]['blob_hash'])
 
         # check reflector to see if it has hashes
-        out = reflector.get_blob_hashes()
+        out = lbrynets['reflector'].get_blob_hashes()
         self.assertTrue(sd_hash in out)
         self.assertTrue(blob_hash in out)
 
         # test to see if we can get peers from the dht with the hash
-        out = dht.get_peers_for_hash({'blob_hash':sd_hash})
+        out = lbrynets['dht'].get_peers_for_hash({'blob_hash':sd_hash})
         self.assertEqual(2, len(out))
 
         # test to see if we can download from dht
-        out = dht.get({'name':'testname'})
+        if key_fee != 0:
+            # send key fee (plus additional amount to pay for tx fee) to dht if necessary
+            self._send_from_lbrycrd(key_fee+1,lbrynets['dht'])
+
+        out = lbrynets['dht'].get({'name':claim_name})
         self.assertEqual('/data/Downloads/FAQ.md',out['path'])
         self.assertEqual(sd_hash, out['stream_hash'])
 
         # check to see if dht has the downloaded hashes
-        out = dht.get_blob_hashes()
+        out = lbrynets['dht'].get_blob_hashes()
         self.assertTrue(sd_hash in out)
         self.assertTrue(blob_hash in out)
+
+        # test to see if lbrynet received key fee
+        if key_fee != 0:
+            self._wait_for_lbrynet_sync()
+            self._increment_blocks(6)
+            self._wait_till_balance_equals(lbrynets['lbrynet'],balance_before_key_fee+key_fee)
 
         # TODO: we should log into the dht docker instance and check for file presense and diff it against original
 
@@ -205,10 +253,11 @@ if __name__ == '__main__':
     out,err=shell_command('docker-compose rm -f')
     out,err=shell_command('docker-compose build')
 
-    out,err=shell_command('docker-compose up > tmp.log&')
+    out,err=shell_command('docker-compose up > {}&'.format(DOCKER_LOG_FILE))
     # Wait for docker containter start up
     # TODO: we should retry connections until they can be reached here
     # instead of sleeping
     time.sleep(60)
     unittest.main()
+
 
--- sync_to_master.sh
+++ sync_to_master.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # Sync all lbry repos to master
+# if supplied with argument, sync to that tag instead 
+
+if [ $# -ge 1 ]
+then
+    tag=master
+else
+    tag=$1
+fi
 
 # these commit changes the address prefix in lbryum and lbryum-server
 # to be in regtest settings, and are cherry-picked on top of master
 LBRYUM_ADDR_PREFIX=adad3604943da278b693ae71a5f317165b1c99b8
 LBRYUM_SERVER_ADDR_PREFIX=1ea2f3c56e7f8b8deebcc05292fef787289a3a61
 
-(cd lbrynet/lbry; git fetch origin; git checkout origin/master)
-(cd lbrynet/lbryum; git fetch origin; git checkout origin/master; git cherry-pick $LBRYUM_ADDR_PREFIX; git cherry-pick $LBRYUM_DIFF_TARGET_FIX)
-(cd lbryum-server/lbryum-server; git fetch origin; git checkout origin/master; git cherry-pick $LBRYUM_SERVER_ADDR_PREFIX)
-(cd lighthouse/lighthouse; git fetch origin; git checkout origin/master)
-(cd lighthouse/lbry; git fetch origin; git checkout origin/master)
+(cd lbrynet/lbry; git fetch origin; git checkout origin/$tag)
+(cd lbrynet/lbryum; git fetch origin; git checkout origin/$tag; git cherry-pick $LBRYUM_ADDR_PREFIX; git cherry-pick $LBRYUM_DIFF_TARGET_FIX)
+(cd lbryum-server/lbryum-server; git fetch origin; git checkout origin/$tag; git cherry-pick $LBRYUM_SERVER_ADDR_PREFIX)
+(cd lighthouse/lighthouse; git fetch origin; git checkout origin/$tag)
+(cd lighthouse/lbry; git fetch origin; git checkout origin/$tag)
