d79093f40a9e884987b7505b429e1b77f0adc514
Kay Kurokawa <kay@lbry.io>
2017-01-09 16:04:13 -0500

    resyncing to master, seperating the application of regtest patch from sync_to_master.sh script

---
+++ apply_regtest_patch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# these commit changes the address prefix in lbryum and lbryum-server
+# to be in regtest settings, and are cherry-picked on top of master
+LBRYUM_ADDR_PREFIX=adad3604943da278b693ae71a5f317165b1c99b8
+LBRYUM_SERVER_ADDR_PREFIX=1ea2f3c56e7f8b8deebcc05292fef787289a3a61
+
+(cd lbrynet/lbryum; git fetch origin; git cherry-pick $LBRYUM_ADDR_PREFIX)
+(cd lbryum-server/lbryum-server; git fetch origin; git cherry-pick $LBRYUM_SERVER_ADDR_PREFIX)
+
