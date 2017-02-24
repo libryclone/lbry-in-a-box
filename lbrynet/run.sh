a153531d8aa114c5d58df9f032c93b8c52862c47
Kay Kurokawa <kay@lbry.io>
2017-02-23 19:26:07 -0500

    updating lbrynet-daemon run script

--- lbrynet/run.sh
+++ lbrynet/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This is a hack to ensure that lbrynet always starts up after lbryum-server is finished syncing
-sleep 10
-/app/bin/python /app/bin/lbrynet-daemon --log-to-console --no-launch --verbose
+sleep 20
+/app/bin/python /app/bin/lbrynet-daemon --verbose
 
